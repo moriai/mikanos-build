904476198bfd99fe097eeb475ebb7f5ac0ac639b
Satoshi Moriai <satoshi.moriai@gmail.com>
2024-07-14 23:37:59 +0900

    QEMUの起動パラメタを修正
    RTCをlocaltimeにする。
    QEMUの別ホストでの起動に対応する。

--- devenv/run_image.sh
+++ devenv/run_image.sh
@@ -1,4 +1,5 @@
-#!/bin/sh -ex
+#!/bin/sh
+###/bin/sh -ex
 
 if [ $# -lt 1 ]
 then
@@ -15,14 +16,46 @@ then
     exit 1
 fi
 
-cp -p $DEVENV_DIR/OVMF_VARS.fd $DEVENV_DIR/OVMF_DATA.fd
-
-qemu-system-x86_64 \
-    -m 1G \
-    -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd \
-    -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_DATA.fd \
-    -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
-    -device nec-usb-xhci,id=xhci \
-    -device usb-mouse -device usb-kbd \
-    -monitor stdio \
-    $QEMU_OPTS
+if [ "$QEMU_HOST" = "" ]; then
+    cp -p $DEVENV_DIR/OVMF_VARS.fd $DEVENV_DIR/OVMF_DATA.fd
+    qemu-system-x86_64 \
+        -m 1G \
+        -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd \
+        -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_DATA.fd \
+        -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
+        -device nec-usb-xhci,id=xhci \
+        -device usb-mouse -device usb-kbd \
+        -rtc base=localtime \
+        -monitor stdio \
+        $QEMU_OPTS
+else
+    if [ "$QEMU_LOCAL_DIR" = "" -o "$QEMU_REMOTE_DIR" = "" ]; then
+        if QEMU_REMOTE_DIR=$(ssh $QEMU_HOST mktemp -dt qemu); then
+            scp -p $DEVENV_DIR/OVMF_CODE.fd $DEVENV_DIR/OVMF_VARS.fd $DISK_IMG \
+                $QEMU_HOST:$QEMU_REMOTE_DIR
+            CLEANUP_REMOTE_DIR=yes
+        else
+            exit $?
+        fi
+    else
+        cp -p $DEVENV_DIR/OVMF_CODE.fd $DEVENV_DIR/OVMF_VARS.fd $DISK_IMG \
+                $QEMU_LOCAL_DIR
+        sync
+    fi
+    ssh $QEMU_HOST '
+        . .profile;
+        cd '"$QEMU_REMOTE_DIR"';
+        qemu-system-x86_64 \
+            -m 1G \
+            -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
+            -drive if=pflash,format=raw,file=OVMF_VARS.fd \
+            -drive if=ide,index=0,media=disk,format=raw,file='"$DISK_IMG"' \
+            -device nec-usb-xhci,id=xhci \
+            -device usb-mouse -device usb-kbd \
+            -rtc base=localtime \
+            -monitor stdio \
+            '"$QEMU_OPTS"
+    if [ "$CLEANUP_REMOTE_DIR" = yes ]; then
+        ssh $QEMU_HOST 'rm -fr '"$QEMU_REMOTE_DIR"
+    fi
+fi
