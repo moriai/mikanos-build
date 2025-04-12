e245a09a3df09e3ecd02008be420aa7a229cbd1d
Satoshi Moriai <satoshi.moriai@gmail.com>
2025-04-12 17:52:31 +0900

    lld-10 以降で OS やアプリが起動失敗する事象に対処
    lld-10 以降では隣接する PT_LOAD セグメントをバイナリファイル上で
    オーバーラップさせるのがデフォルトとなり、MikanOS ブートローダが
    想定しないバイナリが出力される。そのため、lld 起動オプションに
    -z separate-loadable-segments を追加し、従来通りのバイナリを
    出力させるようにする。

--- devenv/buildenv.sh
+++ devenv/buildenv.sh
@@ -14,6 +14,6 @@ else
     -I$EDK2DIR/MdePkg/Include -I$EDK2DIR/MdePkg/Include/X64 \
     -nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS \
     -DEFIAPI='__attribute__((ms_abi))'"
-    export LDFLAGS="-L$BASEDIR/lib"
+    export LDFLAGS="-z separate-loadable-segments -L$BASEDIR/lib"
 fi
 
