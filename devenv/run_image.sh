d6e0420c790e04f6acfff68e6b948c05419f7dad
Satoshi Moriai <satoshi.moriai@gmail.com>
2026-04-29 15:45:11 +0900

    WSL環境でホストにQEMUがあればそれを使う

--- devenv/run_image.sh
+++ devenv/run_image.sh
@@ -7,19 +7,33 @@ then
 fi
 
 DEVENV_DIR=$(dirname "$0")
-DISK_IMG=$1
+DISK_IMG="$1"
 
-if [ ! -f $DISK_IMG ]
+if [ ! -f "$DISK_IMG" ]
 then
     echo "No such file: $DISK_IMG"
     exit 1
 fi
 
-qemu-system-x86_64 \
+QEMU="qemu-system-x86_64"
+OVMF_CODE="$DEVENV_DIR/OVMF_CODE.fd"
+OVMF_VARS="$DEVENV_DIR/OVMF_VARS.fd"
+
+if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    QEMU_FOR_WIN="/mnt/c/Program Files/qemu/qemu-system-x86_64.exe"
+    if [ -x "$QEMU_FOR_WIN" ]; then
+        QEMU="$QEMU_FOR_WIN"
+        OVMF_CODE="$(wslpath -w $DEVENV_DIR/OVMF_CODE.fd)"
+        OVMF_VARS="$(wslpath -w $DEVENV_DIR/OVMF_VARS.fd)"
+        DISK_IMG="$(wslpath -w $DISK_IMG)"
+    fi
+fi
+
+"$QEMU" \
     -m 1G \
-    -drive if=pflash,format=raw,readonly=on,file=$DEVENV_DIR/OVMF_CODE.fd \
-    -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd \
-    -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
+    -drive if=pflash,format=raw,readonly=on,file="$OVMF_CODE" \
+    -drive if=pflash,format=raw,file="$OVMF_VARS" \
+    -drive if=ide,index=0,media=disk,format=raw,file="$DISK_IMG" \
     -device nec-usb-xhci,id=xhci \
     -device usb-mouse -device usb-kbd \
     -rtc base=localtime \
